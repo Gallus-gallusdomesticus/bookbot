9847916d936e2633680fa4367ba0a4ab714a7d5c
Gallus-gallusdomesticus <stephenefelson1999@gmail.com>
2025-03-01 11:03:47 +1100

    Add function for dictionary sort

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ with open("./books/frankenstein.txt") as f:
     read_data=f.read()
 
 
-from stats import num_each_alpha
+from stats import *
 
-print(num_each_alpha(read_data))
+frank_word_num=num_of_words(read_data)
+frank_alpha_count=alpha_list(read_data)
+
+print("============ BOOKBOT ============")
+print("Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
+print(f"Found {frank_word_num} total words")
+print("--------- Character Count -------")
+for diction in frank_alpha_count:
+    print(f"{diction["alpha"]}: {diction["num"]}")
+print("============= END ===============")
+        
--- stats.py
+++ stats.py
@@ -1,11 +1,14 @@
+#FUNCTION TO COUNT HOW MANY WORDS TOTAL IN THE BOOK
 def num_of_words(text):
     num_words=0
     word_list=text.split()
     for word in word_list:
         num_words+=1
-    return f"{num_words} words found in the document"
+    return num_words
 
-def num_each_alpha(text):
+#FUNCTION TO COUNT HOW MANY EACH LETTER AND SYMBOL IN A BOOK
+#complicated method (mine)
+def num_each(text):
     text_lower=text.lower()
     word_list_lower=list(text_lower)
     word_set_lower=set(word_list_lower)
@@ -17,4 +20,34 @@ def num_each_alpha(text):
                 num_word+=1
         word_dict[word_set]=num_word
     return word_dict
+
+#simple method
+def num_each_simple(text):
+    word_dict={}
+    for char in text.lower():
+        if char in word_dict:
+            word_dict[char]+=1
+        else:
+            word_dict[char]=1
+    return(word_dict)
+
+
+#FUNCTION TO SORT THE DICTIONARY BASED ON HIGHEST CHAR COUNT
+#making the key for the sort function (sorting based on what)
+
+def sort_on(dict):
+    return dict["num"]
+
+def alpha_list(text):
+    list=[]
+    for word in num_each_simple(text):
+        if word.isalpha()==True:
+            list.append({f"alpha":word, f"num":num_each_simple(text)[word]})
+    list.sort(reverse=True, key=sort_on)
+    return list
+
+
+
+
+
 
